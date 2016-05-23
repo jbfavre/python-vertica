3c9dcec0785c7df4a682eb871347aed6be1597b4
Jean Baptiste Favre <debian@jbfavre.org>
2016-05-23 21:13:25 +0200

    Imported Upstream version 0.6.3

--- vertica_python/errors.py
+++ vertica_python/errors.py
@@ -1,14 +1,16 @@
-from __future__ import absolute_import
 
-import exceptions
+
+import sys
+if sys.version_info < (3,):
+    import exceptions
 import re
 
 
-class Error(exceptions.StandardError):
+class Error(Exception):
     pass
 
 
-class Warning(exceptions.StandardError):
+class Warning(Exception):
     pass
 
 
@@ -107,6 +109,10 @@ class VerticaSyntaxError(QueryError):
     pass
 
 
+class MissingSchema(QueryError):
+    pass
+
+
 class MissingRelation(QueryError):
     pass
 
@@ -127,14 +133,20 @@ class InvalidDatetimeFormat(QueryError):
     pass
 
 
+class DuplicateObject(QueryError):
+    pass
+
+
 QUERY_ERROR_CLASSES = {
-    '55V03': LockFailure,
-    '53000': InsufficientResources,
-    '53200': OutOfMemory,
-    '42601': VerticaSyntaxError,
-    '42V01': MissingRelation,
-    '42703': MissingColumn,
-    '22V04': CopyRejected,
-    '42501': PermissionDenied,
-    '22007': InvalidDatetimeFormat
+    b'55V03': LockFailure,
+    b'53000': InsufficientResources,
+    b'53200': OutOfMemory,
+    b'42601': VerticaSyntaxError,
+    b'3F000': MissingSchema,
+    b'42V01': MissingRelation,
+    b'42703': MissingColumn,
+    b'22V04': CopyRejected,
+    b'42501': PermissionDenied,
+    b'22007': InvalidDatetimeFormat,
+    b'42710': DuplicateObject
 }
