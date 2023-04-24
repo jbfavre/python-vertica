72a3faa862e15d7a5decfc45660fda520e1b4d85
Jean Baptiste Favre <debian@jbfavre.org>
2023-04-24 15:32:59 +0200

    New upstream version 1.3.2

--- vertica_python/vertica/connection.py
+++ vertica_python/vertica/connection.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018-2023 Micro Focus or one of its affiliates.
+# Copyright (c) 2018-2023 Open Text.
 # Copyright (c) 2018 Uber Technologies, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -77,7 +77,7 @@ try:
     DEFAULT_USER = getpass.getuser()
 except Exception as e:
     DEFAULT_USER = None
-    print("WARN: Cannot get the login user name: {}".format(str(e)))
+    warnings.warn(f"Cannot get the login user name: {str(e)}")
 
 
 def connect(**kwargs):
